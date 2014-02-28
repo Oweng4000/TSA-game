6e60b8da5a56b2bedcfea24e6e2698b21181a4c2
Owen Anderson <Ugafan05@att.net>
2014-02-28 09:03:38 -0500

    Game
    Okay, so now mr. cube changes the color of other cubes, not himself. I
    also figured out how to attach the camera and light to mrcube so they
    follow him. We just need levels and textures now.

--- TSA/Assets/color.js
+++ TSA/Assets/color.js
@@ -30,65 +30,15 @@ lockPos = 0;
 function Update () {
 //y = transform.position.y;
 
-if(Input.GetKey('q')&&r<1.0) {
- r+=amount*Time.deltaTime;
-}
 
-
-if(Input.GetKey('a')&&r>0.0) {
-r-=amount*Time.deltaTime;
-}
-
-if(Input.GetKey('w')&&g<1.0){ 
-g+=amount*Time.deltaTime;
-
-}
-
-if(Input.GetKey('s')&&g>0.0) {
-g-=amount*Time.deltaTime; 
-}
-
-
-if(Input.GetKey('e')&&b<1.0) {
-b+=amount*Time.deltaTime;
-}
-
-
-if(Input.GetKey('d')&&b>0.0) {
-b-=amount*Time.deltaTime;
-}
-
-if(r>=1.0) { 
-r=1.0; 
-}
-if(g>= 1.0) {
-g=1.0; 
-}
-if(b>=1.0) {
-b =1.0;
-}
-if(r<=0.0) {
-r=0.0;
-}
-if(b<=0.0) {
-b=0.0;
-}
-if(g<=0.0) {
-g=0.0;
-}
-if(Input.GetKey("r")) {
-r=0.5;
-g=0.5;
-b=0.5;
-}
 if(Input.GetKey("right")) {
 transform.Translate(Vector3.right*speed*Time.deltaTime); 
-Camera.mainCamera.transform.Translate(Vector3.right*speed*Time.deltaTime);
+//Camera.mainCamera.transform.Translate(Vector3.right*speed*Time.deltaTime);
 //Spotlight.transform.Translate(Vector3.right*Time.deltaTime);
 }
 if(Input.GetKey("left")) {
 transform.Translate(Vector3.left*speed*Time.deltaTime);
-Camera.mainCamera.transform.Translate(Vector3.left*speed*Time.deltaTime);
+//Camera.mainCamera.transform.Translate(Vector3.left*speed*Time.deltaTime);
 //Spotlight.transform.Translate(Vector3.right*Time.deltaTime);
 }
 if(Input.GetKey("space")){
@@ -100,7 +50,7 @@ if(Input.GetKey("space")){
 if(y<-10) {
 transform.position = Vector3(-4.386902e-05,-2.193451e-05,-2.193451e-05);
 rigidbody.velocity = Vector3(0, 0, 0);
-Camera.mainCamera.transform.position= Vector3(0, 0.5483226, -8.4856);
+//Camera.mainCamera.transform.position= Vector3(0, 0.5483226, -8.4856);
 rigidbody.rotation=Quaternion.Euler(Vector3(0,0,0));
 
 }
@@ -108,7 +58,7 @@ rigidbody.rotation=Quaternion.Euler(Vector3(0,0,0));
   //jumping = false;
   //}
 
-gameObject.renderer.material.SetColor("_Color",Color(r,g,b));
+
 y = transform.position.y; 
 //Debug.Log("blue = " +b); 
 //Debug.Log("green = " +g); 
---
+++ TSA/Assets/colorchangeforcubes.js
@@ -0,0 +1,73 @@
+﻿#pragma strict
+static var  r : float;
+static var  g : float;
+static var  b :  float;
+var amount: float; 
+
+function Start () {
+r = 0; 
+g = 1; 
+b = 1; 
+amount = 0.5;
+gameObject.renderer.material.SetColor("_Color",Color.cyan);
+}
+
+function Update () {
+
+
+}
+function OnCollisionStay() {
+Debug.Log("Helllo");
+if(Input.GetKey('q')&&r<1.0) {
+ r+=amount*Time.deltaTime;
+}
+
+
+if(Input.GetKey('a')&&r>0.0) {
+r-=amount*Time.deltaTime;
+}
+
+if(Input.GetKey('w')&&g<1.0){ 
+g+=amount*Time.deltaTime;
+
+}
+
+if(Input.GetKey('s')&&g>0.0) {
+g-=amount*Time.deltaTime; 
+}
+
+
+if(Input.GetKey('e')&&b<1.0) {
+b+=amount*Time.deltaTime;
+}
+
+
+if(Input.GetKey('d')&&b>0.0) {
+b-=amount*Time.deltaTime;
+}
+
+if(r>=1.0) { 
+r=1.0; 
+}
+if(g>= 1.0) {
+g=1.0; 
+}
+if(b>=1.0) {
+b =1.0;
+}
+if(r<=0.0) {
+r=0.0;
+}
+if(b<=0.0) {
+b=0.0;
+}
+if(g<=0.0) {
+g=0.0;
+}
+if(Input.GetKey("r")) {
+r=0.5;
+g=0.5;
+b=0.5;
+}
+gameObject.renderer.material.SetColor("_Color",Color(r,g,b));
+}
