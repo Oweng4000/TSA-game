24debec1a8bbc68568341da18c7f9ce2df172581
Owen Anderson <Ugafan05@att.net>
2014-03-03 17:35:25 -0500

    booty
    booty booty swag swag

---
+++ TSA/Assets/Message.js
@@ -0,0 +1,7 @@
+﻿static var Str : String;
+Str = "Yolo Swaggins and the Fellowship of the Bling";
+ 
+function OnGUI()
+{
+GUILayout.Label(Str);
+}
---
+++ TSA/Assets/signpost.js
@@ -0,0 +1,60 @@
+﻿#pragma strict
+
+//static var Str : String;
+
+ var b : boolean;
+ var mainText : GUIText;
+ var brosef : GUIText;
+ var f : int;
+
+ 
+function Start () {
+//Str = "Yolo Swaggins and the Fellowship of the Bling";
+b=false;
+mainText.text = "swag";
+//booty = "booty";
+f=0;
+}
+
+function Update () {
+
+//booty;
+if(f==0) 
+{
+mainText.text= "Hello and Welcome to Mr. Cube!!!!!";
+}
+if(f==1)
+{
+mainText.text = "Use left and right arrow keys to move";
+
+}
+if(f==2) 
+{
+mainText.text = "Use space to jump!";
+}
+if(f==3)
+{
+mainText.text = "In front of you is a cube. Jump up there to learn more.";
+}
+
+
+mainText.fontSize = 25;
+mainText.pixelOffset.x=0;
+//booty.active;
+mainText.enabled = b;
+brosef.enabled = b;
+if(Input.GetKeyDown(KeyCode.LeftShift)) {
+f+=1;
+}
+Debug.Log(f);
+}
+function OnTriggerEnter() {
+
+b=true;
+}
+function OnTriggerExit() {
+b=false;
+//booty.enabled = false;
+}
+
+ 
